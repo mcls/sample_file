9fb1627c38bc4a1d82b63514d8a9d89a14f5e2f1
Maarten Claes <maartencls@gmail.com>
2013-05-25 15:23:17 +0200

    Define class methods on SampleFile to easily generate sample files

--- lib/sample_file.rb
+++ lib/sample_file.rb
@@ -4,4 +4,21 @@ require "sample_file/image"
 require "sample_file/video"
 
 module SampleFile
+  class << self
+    def image(type='png')
+      Image.new.file(type)
+    end
+
+    def image_path(type='png')
+      Image.new.file_path(type)
+    end
+
+    def video(type='h264')
+      Video.new.file(type)
+    end
+
+    def video_path(type='h264')
+      Video.new.file_path(type)
+    end
+  end
 end
