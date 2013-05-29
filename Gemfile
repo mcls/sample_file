831c21154e9f7f996fa0f11c24dc6e9f06ca0007
Maarten Claes <maartencls@gmail.com>
2013-05-29 19:38:34 +0200

    Add coveralls gem

--- Gemfile
+++ Gemfile
@@ -2,3 +2,7 @@ source 'https://rubygems.org'
 
 # Specify your gem's dependencies in sample_file.gemspec
 gemspec
+
+group :test do
+  gem 'coveralls', require: false
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,9 @@
 require 'rubygems'
 require 'bundler/setup'
-require 'sample_file'
+require 'coveralls'
+Coveralls.wear!
 
+require 'sample_file'
 require 'image_spec'
 
 Dir["./spec/support/**/*.rb"].sort.each {|f| require f}
